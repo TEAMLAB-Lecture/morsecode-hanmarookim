51878d96265cd3bb54b9c07611741ee3ef862b04
Maroo KH <>
2021-01-21 20:28:15 +0900

    update regex

--- morsecode.py
+++ morsecode.py
@@ -146,6 +146,7 @@ def get_cleaned_english_sentence(raw_english_sentence):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     result = re.sub('[.,!?]', '', raw_english_sentence)
+    result = re.sub('[ ]+', ' ', result)
     return result.strip()
     # ==================================
 
@@ -255,7 +256,7 @@ def encoding_sentence(english_sentence):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     sentence = get_cleaned_english_sentence(english_sentence)
-    result = list(map(lambda x: encoding_character(x) if x != ' ' else ' ', sentence))
+    result = list(map(lambda x: encoding_character(x) if x != ' ' else '', sentence))
     return ' '.join(result)
     # ==================================
 
